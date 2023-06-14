9da4e541cd8c617460f0fe4184ffef6bfa3be592
kavyakorivi <kavyakorivi005@gmail.com>
2023-06-13 20:41:34 -0400

    onlyuser

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const path = require('path');
+
+const userRoutes = require('./server/routes/user');
+
+mongoose.connect(process.env.dbURL)
+ .then(console.log("DB Connected!!"))
+ .catch(error => console.log(error));
 
 app.use(express.json());
 
@@ -13,6 +21,8 @@ app.use(function(req, res, next){
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
     next();
 });
+
+app.use('/user',userRoutes);
 
 const PORT =process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server started on port ${PORT}!'));
